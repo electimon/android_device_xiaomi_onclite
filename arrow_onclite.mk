9cc699690dee9a913a38558e841afaa8501d08a6
Erfan Abdi <erfangplus@gmail.com>
2019-09-11 17:36:48 +0600

    onclite: Add updatable_apex.mk

--- arrow_onclite.mk
+++ arrow_onclite.mk
@@ -15,6 +15,9 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 # Inherit some ArrowOS stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 720
 IS_PHONE := true
