55f74663c5ae840208d1961e1314a98372e4f89d
Gagan Malvi <malvigagan@gmail.com>
2020-02-14 19:38:57 +0530

    Arrow bringup, changed hw crypto

--- device.mk
+++ device.mk
@@ -194,6 +194,7 @@ PRODUCT_PACKAGES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee \
     vendor.qti.hardware.cryptfshw@1.0-service-ioctl-qti
 
 # IMS
