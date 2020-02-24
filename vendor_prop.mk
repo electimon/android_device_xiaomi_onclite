582bc3f508b7098c363eaffd7e622573e971ab5d
SunnyRaj84348 <sunnyraj7461@gmail.com>
2020-02-25 05:23:57 +0530

    onclite: Ram Tweaks

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -248,3 +248,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # WiFi calling
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.wfc_avail_ovr=1
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.vendor.qti.sys.fw.bservice_limit=5 \
+    ro.vendor.qti.sys.fw.bservice_age=5000
+
+# RAM Tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
