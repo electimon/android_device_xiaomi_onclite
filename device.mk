40abed7cc2052675fc1f91a4fc6b72a27f73e771
mylove90 <engm.saad1990@gmail.com>
2020-03-21 19:08:43 +0200

    Revert "onclite: Rootdir: Introduce 'SafailNet'"
    This reverts commit 9333f21b61a558243131a0f5fbafb82a853b1674.

--- device.mk
+++ device.mk
@@ -304,8 +304,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc \
-    init.safailnet.rc
+    ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
